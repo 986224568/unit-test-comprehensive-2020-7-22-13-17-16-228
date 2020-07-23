874d44cf31c7b19a08e92fe4ea3f56fba8d9653b
986224568 <986224568@qq.com>
2020-07-23 20:43:01 +0800

    [guess number][Domingo] feature:pass: should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234;

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -3,11 +3,29 @@ package example;
 public class GuessNumber {
     private int[] answer;
 
-    public GuessNumber(int[] answer) {
-        this.answer = answer;
+    public GuessNumber(GenerateRandomNum generateRandomNum) {
+        this.answer = generateRandomNum.generateRandomNum();
     }
 
-    public String guess(int[] nums) {
-        return "";
+    public String guess(int[] guessNum) {
+        int A = 0;
+        int B = 0;
+        for (int num : answer) {
+            for (int gNum : guessNum) {
+                if (num == gNum) {
+                    A++;
+                }
+            }
+        }
+        B = 4 - A;
+        return String.format("%dA%dB", A, B);
+    }
+
+    public int[] generateRandom() {
+        return new int[]{1, 2, 3, 4};
+    }
+
+    public boolean isValid(int[] answer) {
+        return false;
     }
 }
--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -1,17 +1,26 @@
 package example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class GuessNumberTest {
     @Test
     void should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234() {
         //given
         int[] answer = {1,2,3,4};
+        GenerateRandomNum generateRandomNum = Mockito.mock(GenerateRandomNum.class);
+        when(generateRandomNum.generateRandomNum()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(generateRandomNum);
         int[] inputGuess = {1,2,3,4};
         //when
-        GuessNumber guessNumber = new GuessNumber(answer);
         String result = guessNumber.guess(inputGuess);
         //then
         assertEquals("4A0B",result);

365474a66cb9f33139e57bd80b26686e41d94c44
986224568 <986224568@qq.com>
2020-07-27 22:18:45 +0800

    [guess number][Domingo] refactor: separate Main function in Main.java

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -4,21 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        GenerateRandomNum generateRandomNum = new GenerateRandomNumBer();
-        GuessNumber guessNumber = new GuessNumber(generateRandomNum);
-        Scanner sc = new Scanner(System.in);
-        int[] guessNum = new int[4];
+        int[] guessNum = null;
         int times = 6;
         while (times > 0){
-            for (int i = 0; i < 4; i++) {
-                guessNum[i] = sc.nextInt();
-            }
-            if(guessNumber.isValid(guessNum)){
-                guessNumber.guess(guessNum);
-            }else{
-                System.out.println("Wrong Input，Input again");
-            }
+            guessNum = inputGuessNum();
+            guessNumGame(guessNum);
             times--;
+        }
+    }
+
+    private static int[] inputGuessNum() {
+        int[] guessNum = new int[4];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 4; i++) {
+            guessNum[i] = sc.nextInt();
+        }
+        return guessNum;
+    }
+
+    private static void guessNumGame(int[] guessNum) {
+        GenerateRandomNum generateRandomNum = new GenerateRandomNumBer();
+        GuessNumber guessNumber = new GuessNumber(generateRandomNum);
+        if(guessNumber.isValid(guessNum)){
+            guessNumber.guess(guessNum);
+        }else{
+            System.out.println("Wrong Input，Input again");
         }
     }
 }
